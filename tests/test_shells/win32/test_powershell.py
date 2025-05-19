77e84b126ef87be6e990d5912d6b5429a5cf5f14
David Ellis <ducksual@gmail.com>
2025-05-19 18:20:12 +0100

    Powershell tests

--- tests/test_shells/win32/test_powershell.py
+++ tests/test_shells/win32/test_powershell.py
@@ -21,3 +21,57 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 from __future__ import annotations
+
+from unittest.mock import patch
+
+from ducktools.pytui.shells import _core as core, powershell
+
+
+POWERSHELL_PATH = R"C:\Program Files\PowerShell\7\pwsh.exe"
+WINDOWS_POWERSHELL_PATH = R"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+
+def test_registered():
+    assert powershell.PowerShell.bin_name in core.Shell.registry
+    assert powershell.WindowsPowerShell.bin_name in core.Shell.registry
+
+    assert (powershell.PowerShell
+            is core.Shell.registry[powershell.PowerShell.bin_name])
+    assert (powershell.WindowsPowerShell
+            is core.Shell.registry[powershell.WindowsPowerShell.bin_name])
+
+
+def test_frompath():
+    assert (core.Shell.from_path(POWERSHELL_PATH)
+            == powershell.PowerShell(POWERSHELL_PATH))
+    assert (core.Shell.from_path(WINDOWS_POWERSHELL_PATH)
+            == powershell.WindowsPowerShell(WINDOWS_POWERSHELL_PATH))
+
+
+def test_get_venv_shell_command():
+    venv = R"C:\Users\David\Source\result\.venv"
+    path = (
+        R"C:\Users\David\Source\result\.venv\Scripts;"
+        R"C:\Windows\system32;"
+        R"C:\Users\David\.local\bin;"
+        R"C:\Program Files\Github CLI"
+    )
+    prompt = "pytui: .venv"
+
+    env = {
+        "PYTUI_PATH": path,
+        "PYTUI_VIRTUAL_ENV": venv,
+        "PYTUI_VIRTUAL_ENV_PROMPT": prompt,
+    }
+
+    with patch("ducktools.pytui.shells.powershell.get_shell_script") as script_mock:
+        def identity(x): return x
+
+        script_mock.side_effect = identity
+
+        shell = powershell.PowerShell(POWERSHELL_PATH)
+
+        cmd, env_updates = shell.get_venv_shell_command(env)
+
+        assert cmd == [POWERSHELL_PATH, "-NoExit", "activate_pytui.ps1"]
+        assert env_updates == {}
