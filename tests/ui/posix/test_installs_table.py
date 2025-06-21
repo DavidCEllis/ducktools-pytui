d6f1dd90402fa54be34dcbed256c95c7cbb015c8
David C Ellis <ducksual@gmail.com>
2025-06-21 20:44:15 +0100

    Add rerun-failures and mark the flaky test

--- tests/ui/posix/test_installs_table.py
+++ tests/ui/posix/test_installs_table.py
@@ -7,20 +7,14 @@ from ducktools.pytui.ui import ManagerApp, MANAGED_BY_MAPPING
 
 from textual.worker import WorkerFailed
 
-flaky_test = pytest.mark.xfail(
-    reason="Test sometimes fails with no screen", 
-    strict=False,
-    raises=WorkerFailed,
-)
 
-
-@flaky_test
+@pytest.mark.flaky(reruns=5)
 async def test_runtime_table(runtimes):
     app = ManagerApp()
     async with app.run_test() as pilot:
         row_data = [app._runtime_table.get_row_at(i) for i in range(0, app._runtime_table.row_count)]
         row_data.sort(key=itemgetter(3))  # Sort by Path
-      
+
         expected = [
             [
                 py.version_str if py.implementation_version_str == py.version_str else f"{py.version_str} / {py.implementation_version_str}",
@@ -32,4 +26,3 @@ async def test_runtime_table(runtimes):
         ]
         expected.sort(key=itemgetter(3))
         assert row_data == expected
-        
